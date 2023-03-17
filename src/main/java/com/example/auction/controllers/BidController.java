f53a262c87c20cef1a6e61632dc2677dafc35366
Alexandra <alexandravolchihina@yandex.ru>
2023-03-17 23:30:21 +0300

    done!

--- src/main/java/com/example/auction/controllers/BidController.java
+++ src/main/java/com/example/auction/controllers/BidController.java
@@ -1,4 +1,10 @@
 package com.example.auction.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("bid")
 public class BidController {
+
 }
--- src/main/java/com/example/auction/services/LotService.java
+++ src/main/java/com/example/auction/services/LotService.java
@@ -1,4 +1,10 @@
 package com.example.auction.services;
 
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
 public class LotService {
 }
